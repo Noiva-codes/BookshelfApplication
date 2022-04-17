f98e4bc81a7057006f259f52ddbb182fd56d9d5b
javonholmes <43739121+javonholmes@users.noreply.github.com>
2022-04-17 17:57:17 -0400

    Make large changes to Controller class: Added: (all internal & complete) saveBookList, writeBookList, printBookList and saveBook functions Updated: addBook function to be able to correctly add books to our internal Book List. .gitignore updated to ignore local saves.

--- src/main/java/com/application/bookshelf/bookshelfapplication/BookshelfController.java
+++ src/main/java/com/application/bookshelf/bookshelfapplication/BookshelfController.java
@@ -1,6 +1,8 @@
 package com.application.bookshelf.bookshelfapplication;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
@@ -14,9 +16,16 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookshelfController {
-
+    // One HashMap to control the bookName - File Path connection
+    // HashMap handles duplicates, so we don't have to check for that.
+    HashMap<String, File> bookList = new HashMap<>();
 
     @FXML
     private Button addButton;
@@ -24,6 +33,8 @@ public class BookshelfController {
     @FXML
     private AnchorPane basePane;
 
+    //gridPane can be interacted typically by calling for the index and rows
+    //of the ImageViews it contains
     @FXML
     private GridPane gridPane;
 
@@ -44,20 +55,58 @@ public class BookshelfController {
         // FileChooser beginnings right there - review FileChooser when stuck
         //Stage mainStage = (Stage) basePane.getScene().getWindow();
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
+        fileChooser.setTitle("Discover a Book!");
         fileChooser.getExtensionFilters().addAll(
-                // TODO: Change Extension filter to pdf files only
-                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
-                new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+                new FileChooser.ExtensionFilter("PDF Files", "*.pdf"),
                 new FileChooser.ExtensionFilter("All Files", "*.*"));
-         // new Stage() also works here.
-        File selectedFile = fileChooser.showOpenDialog(new Stage());
+        File selectedFile = fileChooser.showOpenDialog(new Stage());  // new Stage() also works here.
         // TODO: If statement - should get the path of the PDF file when selected, if not null;
-        // Also add the file path to a list with all our book objects.
-        /* if (selectedFile != null) {
-            mainStage.display(selectedFile);
-        } */
+        // Also add the file path to a HashMap with all our book objects.
+         if (selectedFile != null) {
+            saveBook(selectedFile.getName(), selectedFile);
+        }
     }
 
+    void saveBook(String bookName, File bookPath) {
+        // Save Book to HashMap
+        bookList.put(bookName, bookPath); //add in order of key:value;
+        saveBookList();
+    }
+
+    // This should write to a file that saves all of our books on our computer.
+    void saveBookList() {
+        //Write to a file that saves all of our books on our computer.
+        File saves = new File("/saves/"); //path for creating directory
+        File savedBooks = new File("./saves/saves.bsa"); //path for creating file
+        try {
+            //make a directory, create a new file, and call our function to write to the new file
+            saves.mkdir();
+            savedBooks.createNewFile();
+            //Since all things have worked, write to the file we created.
+            writeBookList(savedBooks.toPath());
+
+        // TODO: Make catches create a popup dialog that informs the issue to the user.
+        } catch(SecurityException se) {
+            System.out.println("Save file not created. Check your security manager to allow this program to work.");
+        } catch(IOException ioe) {
+            System.out.println("I/O Exception has occurred. Not sure why this happened.");
+        }
+
+    }
+
+    private void writeBookList(Path fileToWrite) throws IOException {
+        String bookEntry;
+        for (Map.Entry<String, File> books : bookList.entrySet()) {
+            bookEntry = books.getKey() + ":" + books.getValue().toString();
+            Files.write(fileToWrite, bookEntry.getBytes());
+        }
+
+    }
+
+    public void printBookList() {
+        for (Map.Entry<String, File> books : bookList.entrySet()) {
+            System.out.println("Book Name = " + books.getKey() +
+                    ", Path to file = " + books.getValue());
+        }
+    }
 }
--- src/main/java/com/application/bookshelf/bookshelfapplication/PDFReader.java
+++ src/main/java/com/application/bookshelf/bookshelfapplication/PDFReader.java
@@ -1,7 +1,11 @@
 package com.application.bookshelf.bookshelfapplication;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+/*
+The PDFReader class is created to handle operations involving PDF files and to be able to
+assists in the creation of /BitMap Images/ for the rest of the application.
 
+*/
 public class PDFReader {
 
     public PDFReader() {
